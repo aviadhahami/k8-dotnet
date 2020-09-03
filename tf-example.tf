413a372c0b15c915e559c9423dc0b91ff8bb52d3
AviadH <aviad.hahami@snyk.io>
2020-09-04 00:16:06 +0300

    updated tf

--- tf-example.tf
+++ tf-example.tf
@@ -1,17 +1,13 @@
-{
-  "resource": [
-    {
-      "aws_cloudtrail": [
-        {
-          "foobar": [
-            {
-              "enable_logging": false,
-              "name": "tf-trail-foobar",
-              "s3_bucket_name": "bucket"
-            }
-          ]
-        }
-      ]
-    }
-  ]
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound from anywhere"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
+
